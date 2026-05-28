c083da93a17f1c9a8947dbc7dddc290547fe6ead
zecravers <azmizainabb@gmail.com>
2026-05-28 19:29:43 +0700

    update favicon and profile

--- resources/views/kegiatan/akan.blade.php
+++ resources/views/kegiatan/akan.blade.php
@@ -2,6 +2,7 @@
 <html lang="id">
 <head>
 <meta charset="UTF-8">
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>AGENDA AKAN DATANG</title>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
